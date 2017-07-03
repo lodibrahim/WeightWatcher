b93841ad57bdd59e40d8a192acc909c4b8ef579a
lodiz <lodiz@DESKTOP-JE0L4NK>
2017-07-03 01:27:59 -0400

    Modified xpath

--- src/test/java/com/sample/page/HomePage.java
+++ src/test/java/com/sample/page/HomePage.java
@@ -10,8 +10,8 @@ public class HomePage {
 
 
 	WebDriver driver;
-	By thirdItem = By.xpath("//div[@class='itemContent']/span[3]");
-	By fifthItem = By.xpath("//div[@class='itemContent']/span[5]");
+	By thirdItem = By.xpath("//div[@class='”itemContent”']/span[3]");
+	By fifthItem = By.xpath("//div[@class='”itemContent”']/span[5]");
 
 
 	public HomePage(WebDriver driver){
@@ -19,11 +19,11 @@ public class HomePage {
 	}
 
 	public String printFdSrvg(int index) {
-		return driver.findElement(By.xpath("//div[@class='itemContent']/span[" + index + "]")).getText();
+		return driver.findElement(By.xpath("//div[@class='”itemContent”']/span[" + index + "]")).getText();
 
 	}
 	public int getNumberOfTages(String tagName){
-		List<WebElement> items=driver.findElements(By.xpath("//div[@class='itemContent']/"+tagName));
+		List<WebElement> items=driver.findElements(By.xpath("//div[@class='”itemContent”']/"+tagName));
 		return items.size();
 	}
 	public String getThirdItem(){
